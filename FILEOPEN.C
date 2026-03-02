b85e5e1643930ef3604fe62f15bba7f78408f215
Amrutha <amruthaskashyap4@gmail.com>
2026-03-02 15:45:25 +0530

    Add files via upload

--- FILEOPEN.C
+++ FILEOPEN.C
@@ -10,9 +10,9 @@ int main()
     if(file_ptr == NULL)
     {
         printf("Failed to open the file");
-        exit(1);
+	return 1;
     }
-
+    fprintf(file_ptr,"dwdwdwdd");
     printf("File opened successfully");
 
     return 0;
